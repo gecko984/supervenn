6fdb73626d916873de7248102610b63d989529eb
gecko984 <gecko984@gmail.com>
2020-02-24 13:52:50 +0300

    controlled randomization for consistent results

--- supervenn/_algorithms.py
+++ supervenn/_algorithms.py
@@ -120,11 +120,11 @@
     produced from the unperturbed similarities matrix). The permutation that yields the smallest value
     of count_runs_of_ones_in_rows(arr[:, permutation]) is returned.
 """
+from collections import defaultdict
+import datetime
+from itertools import permutations
 
 import numpy as np
-from collections import defaultdict
-from itertools import permutations, product
-
 
 HUGE_NUMBER = 1e10 # can fail for weighted! FIXME
 DEFAULT_MAX_BRUTEFORCE_SIZE = 8
@@ -337,6 +337,7 @@ def run_randomized_greedy_algorithm(arr, row_weights=None, seeds=DEFAULT_SEEDS, 
         if seed == 0:
             noise = np.zeros_like(similarities)
         else:
+            np.random.seed(seed)
             noise = (np.random.uniform(0, 1, size=similarities.shape) < noise_prob).astype(int)
 
         np.fill_diagonal(noise, 0)
@@ -396,6 +397,7 @@ def get_permutations(chunks, composition_array, chunks_ordering='minimize gaps',
             permutation = np.argsort(case['array'].sum(0))
         elif case['ordering'] == 'random':
             permutation = np.array(range(len(case['sizes'])))
+            np.random.seed(datetime.datetime.now().microsecond)
             np.random.shuffle(permutation)
         elif case['ordering'] is None:
             permutation = np.array(range(len(case['sizes'])))
