6951df757890547057b98601a05151b5dc134d57
gecko984 <gecko984@gmail.com>
2020-08-07 17:18:43 +0300

    import all

--- supervenn/__init__.py
+++ supervenn/__init__.py
@@ -1,6 +1,2 @@
-from supervenn._algorithms import (
-    get_chunks_and_composition_array,
-    get_permutations
-)
-
-from supervenn._plots import supervenn
+from supervenn._algorithms import *
+from supervenn._plots import *
